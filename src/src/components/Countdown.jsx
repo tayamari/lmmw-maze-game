d3e11f04670c8db3d61bf298788c1cbb85283959
elephino <colin.snyder@gmail.com>
2025-08-23 12:34:39 -0400

    Minimal, static countdown timer component (#7)
    - Countdown component in .jsx file
    - Current un-styled
    - Removed boilerplate Vite/React code
    - Imported countdown component into App

---
+++ src/src/components/Countdown.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Countdown() {
+
+
+    return (
+        <div className="countdown">
+            <h1 className="timerLabel">Timer:</h1>
+            <span className="timer">mm:ss</span>
+        </div>
+    )
+}

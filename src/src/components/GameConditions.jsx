c8ea3ac3b9dcb56e2e422ce09609047b6597cc7d
tayamari <tayloramari.little@gmail.com>
2025-09-13 13:03:21 -0400

    Updated game condition language

--- src/src/components/GameConditions.jsx
+++ src/src/components/GameConditions.jsx
@@ -3,11 +3,11 @@ import Timer from "./Timer";
 export function checkWinningCondition(map, setGameOver) {
     if (!map.some((row) => row.includes(2))) {
         setGameOver(true);
-        alert("Congratulations! You collected all the coins. You win!");
+        alert("Congratulations! You picked the correct remaining ingredients for the elixir and can now make it for Granny B. You win!");
         // Additional logic for restarting the game or other actions
     } else if (!map.some((row) => row.includes(4))) {
         setGameOver(true);
-        alert("Game over !! You collided with the ghost");
+        alert("Game over!! You picked up an in item that does not belong in this elixir...Try again!");
         // Additional logic for restarting the game or other actions
     }
 }
